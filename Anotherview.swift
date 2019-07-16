ba0eb301da40c7bfe2ab5f52f3f696f2a902b165
Gowtham <gowtham.m6795@gmail.com>
2019-07-16 15:10:23 +0530

    Added new file which will be added in project and also add string

---
+++ Anotherview.swift
@@ -0,0 +1,9 @@
+//
+//  Anotherview.swift
+//  Git Tutorial
+//
+//  Created by dJAX on 16/07/19.
+//  Copyright © 2019 dJAX. All rights reserved.
+//
+
+import Foundation
--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+let str="Learn Git completly"
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Today I will Learn\(str)")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
